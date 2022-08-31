c062568cfe314c2ef303ef90bb64d1a35759072f
ezequiel maureira <ezequiel.maureira@gmail.com>
2022-08-31 20:54:32 -0300

    creacion header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { Banner2Component } from './header/banner2/banner2.component';
+import { Banner1Component } from './header/banner1/banner1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Banner2Component,
+    Banner1Component
   ],
   imports: [
     BrowserModule,
